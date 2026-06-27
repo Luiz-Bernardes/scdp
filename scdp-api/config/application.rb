1eb6bac5087fec8a1734b992c31a534f37bcee8d
Luiz Henrique <luizhenbernardes@gmail.com>
2026-06-27 14:51:57 -0300

    disable auto generate minitests

--- scdp-api/config/application.rb
+++ scdp-api/config/application.rb
@@ -28,5 +28,21 @@ module ScdpApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Disable rails default minitests auto generation
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: true,
+        controller_specs: false
+
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+
+      g.helper false
+      g.assets false
+    end
   end
 end
