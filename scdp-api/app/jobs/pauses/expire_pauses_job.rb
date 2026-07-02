77ee585d1e39f93126ec1943c37c1b95347edc4e
Luiz Henrique <luizhenbernardes@gmail.com>
2026-07-02 14:18:53 -0300

    expire pauses job + rspec tests

---
+++ scdp-api/app/jobs/pauses/expire_pauses_job.rb
@@ -0,0 +1,47 @@
+module Pauses
+  class ExpirePausesJob < ApplicationJob
+    queue_as :default
+
+    def perform
+      Pause.active
+           .includes(:pause_type, :team)
+           .find_each do |pause|
+
+        next unless pause.pause_type.has_time_limit?
+        next unless expired?(pause)
+
+        expire!(pause)
+      end
+    end
+
+    private
+
+    def expired?(pause)
+      max_duration = pause.pause_type.max_duration_minutes
+
+      return false if max_duration.blank?
+
+      Time.current >= (
+        pause.started_at + max_duration.minutes
+      )
+    end
+
+    def expire!(pause)
+      Pause.transaction do
+        pause.update!(
+          status: :expired,
+          ended_at: Time.current
+        )
+
+        Pauses::FinishPauseService.new(
+          pause: pause
+        ).send(:process_queue)
+
+        Broadcasts::TeamPauseStateService.new(
+          team: pause.team,
+          pause_type: pause.pause_type
+        ).call
+      end
+    end
+  end
+end
