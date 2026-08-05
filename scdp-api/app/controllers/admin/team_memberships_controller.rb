7f8e3aeba7d102d2f24b9537388da07fdaad1793
Luiz Henrique <soumaisluiz@hotmail.com>
2026-08-04 21:19:17 -0300

    admin team memberships controller

---
+++ scdp-api/app/controllers/admin/team_memberships_controller.rb
@@ -0,0 +1,78 @@
+module Admin
+  class TeamMembershipsController < ApplicationController
+    before_action :authorize_manage_teams!
+
+    def index
+      team_memberships = TeamMembership
+        .includes(
+          :user,
+          :team
+        )
+        .order(:id)
+
+      render json: team_memberships.map { |team_membership|
+
+        Admin::TeamMembershipPresenter.new(
+          team_membership: team_membership
+        ).call
+
+      }
+    end
+
+    def show
+      render json:
+        Admin::TeamMembershipPresenter.new(
+          team_membership: team_membership
+        ).call
+    end
+
+    def create
+      team_membership = TeamMembership.create!(
+        team_membership_params
+      )
+
+      render json:
+        Admin::TeamMembershipPresenter.new(
+          team_membership: team_membership
+        ).call,
+        status: :created
+    end
+
+    def update
+      team_membership.update!(
+        team_membership_params
+      )
+
+      render json:
+        Admin::TeamMembershipPresenter.new(
+          team_membership: team_membership
+        ).call
+    end
+
+    def destroy
+      team_membership.destroy!
+
+      head :no_content
+    end
+
+    private
+
+    def team_membership
+      @team_membership ||= TeamMembership
+        .includes(
+          :user,
+          :team
+        )
+        .find(params[:id])
+    end
+
+    def team_membership_params
+      params
+        .require(:team_membership)
+        .permit(
+          :user_id,
+          :team_id
+        )
+    end
+  end
+end
