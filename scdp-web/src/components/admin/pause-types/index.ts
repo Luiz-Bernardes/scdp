b25ed1e080d2e4666076b8af33aa2903b229e0d2
Luiz Henrique <soumaisluiz@hotmail.com>
2026-09-19 07:26:42 -0300

    components admin pause types index

---
+++ scdp-web/src/components/admin/pause-types/index.ts
@@ -0,0 +1,9 @@
+export { PauseTypesPage } from "./pages/PauseTypesPage";
+export { NewPauseTypePage } from "./pages/NewPauseTypePage";
+export { EditPauseTypePage } from "./pages/EditPauseTypePage";
+
+export { PauseTypeForm } from "./form/PauseTypeForm";
+
+export { PauseTypesTable } from "./table/PauseTypesTable";
+export { PauseTypeTableRow } from "./table/PauseTypeTableRow";
+export { DeletePauseTypeButton } from "./table/DeletePauseTypeButton";
